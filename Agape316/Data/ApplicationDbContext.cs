aba84a2580f6c8de551b863af3ac13f565859af9
Debra Hodges <dhodges351@gmail.com>
2023-05-14 15:59:33 -0500

    Remove serviceDependencies.

--- Agape316/Data/ApplicationDbContext.cs
+++ Agape316/Data/ApplicationDbContext.cs
@@ -15,16 +15,5 @@ namespace Agape316.Data
         public DbSet<Event> Event { get; set; }
         public DbSet<EventItem> EventItem { get; set; }
         public DbSet<ApplicationUser>? ApplicationUsers { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-#if DEBUG
-            optionsBuilder.UseSqlServer(@"Server=HODGES-PC\SQLEXPRESS;Database=Agape316;Trusted_Connection=True;MultipleActiveResultSets=true;");
-#else
-            // production connection string
-            optionsBuilder.UseSqlServer(@"Server=174.138.185.50;Database=Agape316;User Id=dhodges351;password=Sbpkjabb@1;Trusted_Connection=False;MultipleActiveResultSets=true;");
-            //optionsBuilder.UseLazyLoadingProxies();
-#endif
-        }
     }
 }
