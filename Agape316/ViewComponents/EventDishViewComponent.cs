fd0015cbce1e4fced980e0b144f12981e1ab4c53
Debra Hodges <dhodges351@gmail.com>
2023-06-18 17:00:31 -0500

    Changes to remove expired events and meal schedules from select lists.

--- Agape316/ViewComponents/EventDishViewComponent.cs
+++ Agape316/ViewComponents/EventDishViewComponent.cs
@@ -28,9 +28,35 @@ namespace Agape316.ViewComponents
             var model = new EventDishModel(_eventService, _eventDishService, eventId, id);
 
             var agapeEvents = _eventService.GetAll().ToList();
-            ViewData["AgapeEventObjects"] = agapeEvents;
+            var filteredAgapeEvents = new List<Event>();
+            
+            // Filter out expired events.
+            if (agapeEvents != null && agapeEvents.Count > 0)
+            {  
+                foreach (var agapeEvent in agapeEvents)
+                {
+                    DateTime date1 = DateTime.Now;                    
+                    DateTime date2 = DateTime.Parse($"{agapeEvent.EventDate.ToShortDateString()} {agapeEvent.EndTime}");
+                    int result = DateTime.Compare(date1, date2);
+                    if (result <= 0)
+                    {
+                        filteredAgapeEvents.Add(agapeEvent);
+                    }                    
+                }
+            }
+            ViewData["AgapeEventObjects"] = filteredAgapeEvents.Count > 0 ? filteredAgapeEvents : agapeEvents;
 
             var agapeEventModels = new List<AgapeEventModel>();
+            if (filteredAgapeEvents != null && filteredAgapeEvents.Count > 0)
+            {
+                foreach (var agapeEvent in filteredAgapeEvents)
+                {
+                    agapeEventModels.Add(new AgapeEventModel { EventId = agapeEvent.Id, Title = agapeEvent.Title });
+                }
+
+                ViewData["AgapeEvents"] = new SelectList(agapeEventModels, "EventId", "Title");
+                return View(model);
+            }
             if (agapeEvents != null && agapeEvents.Count > 0)
             {
                 foreach (var agapeEvent in agapeEvents)
--- Agape316/ViewComponents/MealDeliveryViewComponent.cs
+++ Agape316/ViewComponents/MealDeliveryViewComponent.cs
@@ -27,7 +27,34 @@ namespace Agape316.ViewComponents
             var model = new MealDeliveryModel(_mealScheduleService, _mealDeliveryService, scheduleId, id);
 
             var mealSchedules = _mealScheduleService.GetAll().ToList();
+            var filteredMealSchedules = new List<MealSchedule>();
+
+            if (mealSchedules != null && mealSchedules.Count > 0)
+            {
+                foreach (var mealSchedule in mealSchedules)
+                {
+                    DateTime date1 = DateTime.Now;
+                    DateTime date2 = mealSchedule.EndDate;
+                    int result = DateTime.Compare(date1, date2);
+                    if (result <= 0)
+                    {
+                        filteredMealSchedules.Add(mealSchedule);
+                    }
+                }
+            }
+  
             var mealScheduleModels = new List<MealScheduleModel>();
+            if (filteredMealSchedules != null && filteredMealSchedules.Count > 0)
+            {
+                foreach (var mealSchedule in filteredMealSchedules)
+                {
+                    mealScheduleModels.Add(new MealScheduleModel { Id = mealSchedule.Id, Title = mealSchedule.Title });
+                }
+
+                ViewData["MealSchedules"] = new SelectList(mealScheduleModels, "Id", "Title");
+                return View(model);
+            }
+
             if (mealSchedules != null && mealSchedules.Count > 0)
             {
                 foreach (var mealSchedule in mealSchedules)
